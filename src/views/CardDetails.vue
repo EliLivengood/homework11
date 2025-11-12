3c6ebda78290242c7efe76a09d1e26280c7ae8f6
EliLivengood <elivengo@stu.southtexascollege.edu>
2025-11-12 15:39:46 -0600

    card details finished

--- src/views/CardDetails.vue
+++ src/views/CardDetails.vue
@@ -1,8 +1,34 @@
 <script setup>
 import { useRoute } from 'vue-router'
+import { ref, onMounted, onUnmounted } from 'vue'
+import useAPI from '@/composables/useAPI'
+import { faker } from '@faker-js/faker'
+
+const {fetchEmployee, currentEmployee} = useAPI()
+
+onMounted( async() => {
+    await fetchEmployee(route.params.id)
+})
+
+onUnmounted(() => {
+    currentEmployee.value = null
+})
+
 const route = useRoute()
 </script>
 
 <template>
-    {{route.params.id}}
+    <!-- {{route.params.id}} -->
+     <main>
+        <div v-if="currentEmployee" class="flex flex-col items-center justify-center gap-6">
+            <h1 class = "text-6xl font-bold p-5">{{ currentEmployee.firstName }} {{ currentEmployee.lastName }}</h1>
+            <h1 class = "text-3xl p-5">{{ currentEmployee.title }}</h1>
+            <h1 class = "text-2xl p-5">{{ currentEmployee.userName }}@southtexascollege.edu</h1>
+            <h1 class = "text-2xl p-5">{{ currentEmployee.quote }}</h1>
+
+            <!-- i believe this is rate limited, it was working and stopped showing images half way through -->
+            <!-- <img class ="p-8" :src="faker.image.urlLoremFlickr({category: 'cat'})" /> -->
+            <img class="p-8" :src="faker.image.urlLoremFlickr({category: 'cats', height: 480, width: 960})" />
+        </div>
+     </main>
 </template>
